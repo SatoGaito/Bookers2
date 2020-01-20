4977a04aa555afef316f9244297cb34d900de882
sato gaito <gaito4570@gmail.com>
2020-01-20 16:48:03 +0900

    [Finish] User index show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: 'You have creatad book successfully.'
     else

69396343dfc765f74ad4bfe877830b3cd1db5d88
sato gaito <gaito4570@gmail.com>
2020-01-20 20:48:48 +0900

    [Finish] ensure_correct_user

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:destroy, :edit, :update]
 
   def index
     @books = Book.all
@@ -48,5 +49,12 @@ class BooksController < ApplicationController
   private
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    book = Book.find(params[:id])
+    if current_user.id != book.user_id
+      redirect_to books_path
+    end
   end
 end

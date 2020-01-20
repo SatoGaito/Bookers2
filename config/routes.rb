e7053edbac83f8b4d67e2939b5fb58a06202f3c0
sato gaito <gaito4570@gmail.com>
2020-01-20 21:23:57 +0900

    [Finish] Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'books#index'
+  root 'home#top'
+  get 'about' => 'home#about'
   resources :books
   resources :users, only: [:index, :show, :edit, :update]
 end
